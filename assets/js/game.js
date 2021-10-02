e97887d60b2d63801e4fcc6e60abfe31eba3f18d
Sam Davenport <samueldavenport@att.net>
2021-10-02 17:49:59 -0500

    fixed buggy endgame issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -142,7 +142,7 @@ var endGame = function() {
 
     if (playAgainConfirm) {
         // restart the game
-        startGame();
+        window.alert("Thank you for playing Robot Gladiators! To play gain, refresh the browser.")
     }else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
@@ -151,25 +151,23 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
         );
+        shopOptionPrompt = parseInt(shopOptionPrompt);
         // use switch to carry out action
         switch (shopOptionPrompt) {
-            case "Refill":
-            case "REFILL": 
-            case "refill":
+            case 1:
+            
                 playerInfo.refillHealth();
                 break;
                 
-            case "UPGRADE":
-            case "Upgrade":
-            case "upgrade":
+            case 2:
+            
                 playerInfo.upgradeAttack();
                 break;
                
-            case "leave":
-            case "LEAVE":
-            case "Leave":
+            case 3:
+            
                 window.alert("Leaving the store.");
 
                 // do nothing, so function will end

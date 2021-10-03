3d6fb9baef33fa918fb61fea8143be6b5c470226
Sam Davenport <samueldavenport@att.net>
2021-10-02 20:47:35 -0500

    saved high score to localStorage, done here for the time being

--- assets/js/game.js
+++ assets/js/game.js
@@ -144,13 +144,28 @@ var startGame = function() {
 
 // function to end the entire game
 var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
     // if player is still alive, player wins!
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "! Well done! This shall hereby be recorded in this browser until a new champion shall arise.")
+    } else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ", previously recorded by " + localStorage.name + ". Maybe next time!");
+    }
     if (playerInfo.health > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + " Robo Bucks...  Nice!");
     }else {
         window.alert("You've lost your robot in battle.");
     }
-    window.alert("The game has now ended. Let's see how you did!");
+   
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
